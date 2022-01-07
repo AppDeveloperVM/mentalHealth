616fc164ca112540c9238eb55c91c6497c77e44a
AppDeveloperVM <vicmr1994@gmail.com>
2022-01-07 11:36:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-const Page = () => {
+import React, { useState, useEffect } from 'react'
+import { useRouter, Loading } from 'next/router';
+import { useSession } from "next-auth/react"
+
+import { getSession } from "next-auth/react"
+
+
+const Page = ( req, res ) => {
+    const session = getSession({ req })
+
+    if (session === "authenticated") {
+        return <p>Signed in as {session.user.email}</p>
+    }
+
     return (
-        <div>Index</div>
+        <div>
+        <h1>Index</h1>
+        <div>Hey invitate</div>
+        </div>
     )
 }
+
 
 export default Page
